693f1515bd3afc71768e2632a61d3de9c218b459
hmuar <hmuar34@gmail.com>
2016-09-04 14:35:38 -0400

    watch and regenerate docs, remove docs from repository

--- src/db/collection.js
+++ src/db/collection.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 
+// User
+// ----
+// Main user account. Has access info from other adapters.
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -8,6 +11,10 @@ const userSchema = new mongoose.Schema({
 
 export const User = mongoose.model('User', userSchema, 'users');
 
+// StudentSession
+// --------------
+// Current state of user with app (what note they are on, what stage
+// of response they have with note, remaining note queue etc.)
 const sessionSchema = new mongoose.Schema({
   userID: mongoose.Schema.Types.ObjectId,
   subjects: {},
@@ -17,6 +24,10 @@ export const StudentSession = mongoose.model('StudentSession',
                                      sessionSchema,
                                      'studentsession');
 
+
+// Category
+// --------
+// Main collection of hierarchical pieces to organize notes under
 const catSchema = new mongoose.Schema({
   createdAt: Date,
   ctype: String,
@@ -30,6 +41,12 @@ const catSchema = new mongoose.Schema({
 
 export const Category = mongoose.model('Category', catSchema, 'category');
 
+// Note
+// ----
+// Main unit of information users interact with. Can be info,
+// or question / answer pair user responds to and gets evaluated on.
+// Note is just subset of Category, but use different
+// schema to make it easier and clearer to work with.
 const noteSchema = new mongoose.Schema({
   createdAt: Date,
   ctype: String,
@@ -48,10 +65,12 @@ const noteSchema = new mongoose.Schema({
   directParent: mongoose.Schema.Types.ObjectId,
 });
 
-// Note is just subset of Category, but use different
-// schema to make it easier and clearer to work with.
 export const Note = mongoose.model('Note', noteSchema, 'category');
 
+// Note Record
+// -----------
+// History of user interaction with a given note. Holds factors that inform
+// how to re-schedule note, evaluate how well user understands note
 const noteRecordSchema = new mongoose.Schema({
   userID: mongoose.Schema.Types.ObjectId,
   noteID: mongoose.Schema.Types.ObjectId,
