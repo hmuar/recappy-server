667653da30ceb084e10eab49a143259c9a5105ec
hmuar <hmuar34@gmail.com>
2017-05-13 20:02:28 -0400

    - separate config into dev vs prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,28 @@
-const Config = {
+const BaseConfig = {
   AWSAccessKeyId: 'AKIAJS3Y5RASDJ7EZJ6Q',
   AWSSecretAccessKey: '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',
-  // This is the production token
-  FBToken: 'EAAIkwPmW9ZB4BAAfckvuI9tJklZCO382lc5OVlcWbbUi3Dj1FtQi8Mke3ZBeiKxTZBN8htaDZA40PI6xfY11ZCuKVUPXR1GvpnRPNS7yXTON2SzO5fjfpGFH02NltjUK9rhkYPZB0wgdvF7XxgOWOP7lLomU9OF10MPE7Qu5QilZCwZDZD', //eslint-disable-line
-  // FBToken: 'EAALQvUFfW48BAORZCiRFxYMjRCEkTR09UHwX9iSMvmwGrZBqnZCzMujMxwZBk6BJhqiKbRwI5bObj5iM7DMrOYteqOxr1KuXPWpVKip4ZAbeXlY2XqX0A5l8XZBYodotQ9CSlNIhkte9xwZCAglCW0azhWZB4SK0PnEkDcp9rIoRWAZDZD', //eslint-disable-line
   AWSBucket: 'sadbandit-synapse',
   AWSRegion: 'us-west-1',
-  AuthEndpoint: 'https://41d8e76d.ngrok.io/api/v1/login',
 };
 
-export default Config;
+const ProdConfig = {
+  ...BaseConfig,
+  FBToken: 'EAAIkwPmW9ZB4BAAfckvuI9tJklZCO382lc5OVlcWbbUi3Dj1FtQi8Mke3ZBeiKxTZBN8htaDZA40PI6xfY11ZCuKVUPXR1GvpnRPNS7yXTON2SzO5fjfpGFH02NltjUK9rhkYPZB0wgdvF7XxgOWOP7lLomU9OF10MPE7Qu5QilZCwZDZD', //eslint-disable-line
+  PAGE_ID: '214304282285452',
+};
+
+const DevConfig = {
+  ...BaseConfig,
+  FBToken: 'EAALQvUFfW48BAHrpI7UwU1kjIUgPohW8TaGddz124ZBVirX9gJWzcWXgo8vIOIXp1QlQKWEjR2KDH2FHzPaLKNGSD2TJizyDWqGWaLFds4AZAGiDZCibdkCyiSsZCeRUN0tZBfYJ98vXmB8ZCFeeHWiGsPo9mRQ9mhAjzDpA8hhQZDZD', //eslint-disable-line
+  PAGE_ID: '263306640744019',
+};
+
+// https://synapse-chat.herokuapp.com/api/v1/fbmessage
+
+const isProduction = process.env.NODE_ENV === 'production';
+if (isProduction) {
+  console.log('PROD CONFIG detected.................');
+} else {
+  console.log('DEV CONFIG detected.................');
+}
+export default (isProduction ? ProdConfig : DevConfig);
